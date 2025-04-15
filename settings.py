b83419dd2199de81825722b6378c6ac361bd31e3
Kseniya <kseniya2312@gmail.com>
2025-04-15 15:42:31 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-valid_phone = 'valid phone number'
-valid_login = 'rt_48078311'
-valid_email = 'kseniya2312@gmail.com'
-valid_ls = 'valid account number'
-valid_password = '89992175077Zx'
-
-user_name = 'Точёнова Ксения Геннадьевна'
-
-unregisered_email = 'Super.yulia888@mail.ru'   
