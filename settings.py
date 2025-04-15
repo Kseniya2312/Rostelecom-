fe5b75e476a10362d44ae4d27101b630a888a593
Kseniya <kseniya2312@gmail.com>
2025-04-15 15:04:20 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+valid_phone = 'valid phone number'
+valid_login = 'rtkid_1741463395910'
+valid_email = 'salyresaly@gmail.com'
+valid_ls = 'valid account number'
+valid_password = '123---321Password'
+
+user_name = 'Ресалтваниюсн Саливан'
+
+unregisered_email = 'MSLa@mail.ru'
+
+
+
+    
